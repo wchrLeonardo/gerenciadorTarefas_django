ea01efff5e924314948a7cd3d12708c9923f810f
wicher_leonardo <leonardo.wlferreira@gmail.com>
2024-08-16 17:32:04 -0300

    Vinculando tasks a projects/criando novo template

--- setup/urls.py
+++ setup/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 
-from startask.views import CombinedListView, TaskCreateView
+from startask.views import CombinedListView, ProjectCreateView, TaskCreateView
 # from startask.views import add_project
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", CombinedListView.as_view(), name="task_list"),
-    path("add-project/", TaskCreateView.as_view(), name="add_project"),
+    path("add-project/", ProjectCreateView.as_view(), name="add_project"),
+    path("add-task/", TaskCreateView.as_view(), name="add_task"),
 ]
 
--- startask/views.py
+++ startask/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView
-
+from django.shortcuts import get_object_or_404
 from .models import Task, Project
 # Create your views here.
 
@@ -10,18 +10,47 @@ class CombinedListView(ListView):
     template_name = 'task_list.html'
     context_object_name = 'tasks'
 
+    def get_queryset(self):
+        project_id = self.request.GET.get('project_id')
+        if project_id:
+            return Task.objects.filter(project=project_id)
+        return Task.objects.none()
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['projects'] = Project.objects.all() 
+        context['selected_project_id'] = self.request.GET.get('project_id')
         # context['another_models'] = AnotherModel.objects.all()  
         return context
     
     
-class TaskCreateView(CreateView):
+class ProjectCreateView(CreateView):
     model = Project
     fields = ["name"]
     success_url = reverse_lazy('task_list')
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    fields = ["title", "description"]
+    success_url = reverse_lazy("task_list")
+
+    def form_valid(self, form):
+        project_id = self.request.POST.get("project_id")
+        if project_id:
+            project = get_object_or_404(Project, id=project_id)
+            form.instance.project = project
+        return super().form_valid(form)
+
+
+
     
+
+
+
+
+
+
 # def task_and_project_list(request):
 #     tasks = Task.objects.all()
 #     projects = Project.objects.all()
